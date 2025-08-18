a3553c79fb32d4dcf93b88a63917bbd4074e6b45
nguyenhungAAA <quanghunglenguyen@gmail.com>
2025-08-18 20:32:13 +0700

    style: Add initial scss style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Game from './component/Game'
-
+import Home from './component/Home';
 function App() {
-
   return (
-    <>
-      <Game />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/game' element={<Game />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
